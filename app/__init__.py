3bd7e0ef680843771e4709c53312da953be24ac2
Barasa-Micah <barasamica@gmail.com>
2024-11-30 11:42:28 +0300

    Initialized the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
-import os
-from dotenv import load_dotenv
 
-
-
-#Initiliazing database, migration and JWT manager
 
 db = SQLAlchemy()
-Migrate = Migrate()
-jwt = JWTManager()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
-
-
-    #Loading environment variables from .env file
-    load_dotenv()
-
-    #Loading configuration from config.py
-    app.config.from_object("config.config")
-
-    #Initializing extemnsions with the app
+    app.config.from_object("config.Config")
     db.init_app(app)
-    Migrate.init_app(app, db)
-    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+
+    from .routes import main_bp
+    app.register_blueprint(main_bp)
+
+
+    return app
